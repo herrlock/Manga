36b3628e5c24964e7aaed721bd462d10f0df56ef
herrlock <pokerau@t-online.de>
2015-01-28 23:36:06 +0100

    change errorlog

--- src/dist/Manga.bat
+++ src/dist/Manga.bat
@@ -1,6 +1,5 @@
 @echo off
-echo. >> log/err.log
-echo. >> log/err.log
+echo. > log/err.log
 echo ------------------------ >> log/err.log
 echo %date%%time% >> log/err.log
 bin/Manga.bat 2>>log/err.log
