5b7323138bf85649a6822fe899e0415e4a760c4f
herrlock <pokerau@t-online.de>
2015-01-31 23:37:15 +0100

    update build.gradle and start-script

--- src/dist/Manga.bat
+++ src/dist/Manga.bat
@@ -2,5 +2,5 @@
 echo. > log/err.log
 echo ------------------------ >> log/err.log
 echo %date%%time% >> log/err.log
-java -cp "libs/*" de.herrlock.manga.Ctrl 2>>log/err.log
+java -cp "lib/*" de.herrlock.manga.Ctrl 2>>log/err.log
 if (%ERRORCODE% neq "0") notepad log/err.log
