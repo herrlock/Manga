5bfb859e3c6ff8fdf711f8e85a3ca91ca7c23725
herrlock <pokerau@t-online.de>
2015-02-07 22:10:15 +0100

    fix script, used wrong variable

--- src/dist/Manga.bat
+++ src/dist/Manga.bat
@@ -3,4 +3,4 @@ echo. > log/err.log
 echo ------------------------ >> log/err.log
 echo %date%%time% >> log/err.log
 java -cp "lib/*" de.herrlock.manga.Ctrl 2>>log/err.log
-if (%ERRORCODE% neq "0") notepad log/err.log
+if errorlevel 1 notepad log/err.log
