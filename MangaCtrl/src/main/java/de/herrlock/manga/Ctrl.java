a8beb694dab2ed8ad0711e914a1db87ec22278de
herrlock <pokerau@t-online.de>
2015-03-12 22:59:08 +0100

    replace initial dialog with JavaFX-dialog

--- MangaCtrl/src/main/java/de/herrlock/manga/Ctrl.java
+++ MangaCtrl/src/main/java/de/herrlock/manga/Ctrl.java
@@ -1,44 +1,123 @@
 package de.herrlock.manga;
 
-import javax.swing.JOptionPane;
+import java.util.ResourceBundle;
 
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.geometry.Insets;
+import javafx.scene.Node;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.text.Text;
+import javafx.stage.Stage;
+import de.herrlock.javafx.scene.AbstractScene;
 import de.herrlock.manga.downloader.DialogDownloader;
 import de.herrlock.manga.host.PrintAllHoster;
 import de.herrlock.manga.html.ViewPageMain;
 
-public final class Ctrl {
+public final class Ctrl extends Application {
 
     public static void main( String[] args ) {
-        String message = "Select variant to execute.";
-        String title = "Please select";
-        int optionType = JOptionPane.DEFAULT_OPTION;
-        int messageType = JOptionPane.QUESTION_MESSAGE;
-        Object[] options = {
-            "Download", "Show Hoster", "Create HTML"
+        Application.launch( args );
+    }
+
+    @Override
+    public void start( Stage stage ) throws Exception {
+        AbstractScene scene = new CtrlScene();
+        stage.setScene( scene.getScene() );
+        stage.setTitle( scene.getTitle() );
+        stage.getIcons().addAll( scene.getIcons() );
+        stage.show();
+    }
+}
+
+final class CtrlScene extends AbstractScene {
+
+    private final ResourceBundle i18n = ResourceBundle.getBundle( "de.herrlock.manga.ctrl" );
+    final Text bottomText = new Text();
+
+    CtrlScene() {
+        BorderPane parent = new BorderPane();
+        parent.setPadding( new Insets( 8, 24, 8, 24 ) );
+        parent.setCenter( createCenter() );
+        parent.setBottom( this.bottomText );
+        this.setScene( new Scene( parent ) );
+    }
+
+    private Node createCenter() {
+        EventHandler<MouseEvent> cte = new EventHandler<MouseEvent>() {
+            @Override
+            public void handle( @SuppressWarnings( "unused" ) MouseEvent event ) {
+                CtrlScene.this.bottomText.setText( "" );
+            }
         };
-        int selection = JOptionPane.showOptionDialog( null, message, title, optionType, messageType, null, options, options[0] );
-        switch ( selection ) {
-            case 0:
-                DialogDownloader.execute();
-                break;
-            case 1:
-                PrintAllHoster.execute();
-                break;
-            case 2:
-                ViewPageMain.execute();
-                break;
-            case -1:
-                // X is pressed
-                break;
-            default:
-                System.out.println( "invalid option chosen" );
-                break;
+
+        final String buttonTextPrefix = "button.text.";
+        final String buttonTooltipPrefix = "button.tooltip.";
+        Button startDL = new Button( this.i18n.getString( buttonTextPrefix + "startDL" ) );
+        {
+            startDL.setDefaultButton( true );
+            startDL.setOnAction( startDownloadHandler );
+            startDL.setOnMouseEntered( new SetTextEvent( this.i18n.getString( buttonTooltipPrefix + "startDL" ) ) );
+            startDL.setOnMouseExited( cte );
+        }
+        Button showHosts = new Button( this.i18n.getString( buttonTextPrefix + "showHosts" ) );
+        {
+            showHosts.setOnAction( showHosterHandler );
+            showHosts.setOnMouseEntered( new SetTextEvent( this.i18n.getString( buttonTooltipPrefix + "showHosts" ) ) );
+            showHosts.setOnMouseExited( cte );
+        }
+        Button createHTML = new Button( this.i18n.getString( buttonTextPrefix + "createHTML" ) );
+        {
+            showHosts.setOnAction( createHtmlHandler );
+            createHTML.setOnMouseEntered( new SetTextEvent( this.i18n.getString( buttonTooltipPrefix + "createHTML" ) ) );
+            createHTML.setOnMouseExited( cte );
+        }
+        HBox hbox = new HBox( 8 );
+        hbox.setPadding( new Insets( 8 ) );
+        hbox.getChildren().addAll( startDL, showHosts, createHTML );
+        return hbox;
+    }
+
+    @Override
+    public String getTitle() {
+        return "Please select";
+    }
+
+    private class SetTextEvent implements EventHandler<MouseEvent> {
+        private final String textToSet;
+
+        public SetTextEvent( String textToSet ) {
+            this.textToSet = textToSet;
         }
 
-        System.out.println( "\n---\nfinished" );
+        @Override
+        public void handle( @SuppressWarnings( "unused" ) MouseEvent event ) {
+            CtrlScene.this.bottomText.setText( this.textToSet );
+        }
     }
 
-    private Ctrl() {
-        // nothing to do
-    }
+    private static final EventHandler<ActionEvent> startDownloadHandler = new EventHandler<ActionEvent>() {
+        @Override
+        public void handle( @SuppressWarnings( "unused" ) ActionEvent event ) {
+            DialogDownloader.execute();
+        }
+    };
+    private static final EventHandler<ActionEvent> showHosterHandler = new EventHandler<ActionEvent>() {
+        @Override
+        public void handle( @SuppressWarnings( "unused" ) ActionEvent event ) {
+            PrintAllHoster.execute();
+        }
+    };
+    private static final EventHandler<ActionEvent> createHtmlHandler = new EventHandler<ActionEvent>() {
+        @Override
+        public void handle( @SuppressWarnings( "unused" ) ActionEvent event ) {
+            ViewPageMain.execute();
+        }
+    };
+
 }
