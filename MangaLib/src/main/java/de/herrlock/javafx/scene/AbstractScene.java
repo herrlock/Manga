3384b134d3fcf21885520fbd2efa01286be0179a
herrlock <pokerau@t-online.de>
2015-03-12 22:58:50 +0100

    add support for JavaFX

---
+++ MangaLib/src/main/java/de/herrlock/javafx/scene/AbstractScene.java
@@ -0,0 +1,25 @@
+package de.herrlock.javafx.scene;
+
+import java.util.Arrays;
+import java.util.Collection;
+
+import javafx.scene.Scene;
+import javafx.scene.image.Image;
+
+public abstract class AbstractScene {
+    private Scene scene;
+
+    public Scene getScene() {
+        return this.scene;
+    }
+
+    protected void setScene( Scene scene ) {
+        this.scene = scene;
+    }
+
+    public Collection<Image> getIcons() {
+        return Arrays.asList();
+    }
+
+    public abstract String getTitle();
+}
