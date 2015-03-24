ceffed39e450e9163242d395cd16523b20411694
herrlock <herrlock@t-online.de>
2015-03-24 22:26:27 +0100

    remove main-method

--- MangaLib/src/main/java/de/herrlock/manga/jd/Crawljob.java
+++ MangaLib/src/main/java/de/herrlock/manga/jd/Crawljob.java
@@ -1,29 +1,10 @@
 package de.herrlock.manga.jd;
 
 import java.io.File;
-import java.io.FileOutputStream;
-import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
 import java.util.List;
 
 public class Crawljob {
-
-    private static final File CJ = new File( "./crawljobs/" );
-
-    public static void main( String[] args ) throws Exception {
-        Crawljob c1 = new Crawljob( new File( "./lhtest_0815/00" ), "1" );
-        c1.addCrawljob( "04", "http://a.mfcdn.net/store/manga/13229/01-000.0/compressed/d004.jpg" );
-        c1.addCrawljob( "05", "http://a.mfcdn.net/store/manga/13229/01-000.0/compressed/d005.jpg" );
-        try ( FileOutputStream out = new FileOutputStream( new File( CJ, "./1.crawljob" ) ) ) {
-            out.write( c1.export().getBytes( StandardCharsets.UTF_8 ) );
-        }
-        Crawljob c2 = new Crawljob( new File( "./lhtest_0815/01" ), "2" );
-        c2.addCrawljob( "07", "http://a.mfcdn.net/store/manga/13229/01-001.0/compressed/j007.jpg" );
-        c2.addCrawljob( "08", "http://a.mfcdn.net/store/manga/13229/01-001.0/compressed/j008.jpg" );
-        try ( FileOutputStream out = new FileOutputStream( new File( CJ, "./2.crawljob" ) ) ) {
-            out.write( c2.export().getBytes( StandardCharsets.UTF_8 ) );
-        }
-    }
 
     private final File folder;
     private final String packagenumber;
