aa710f5b13920e49bb07a531617abaf7f4899787
herrlock <herrlock@t-online.de>
2015-03-25 01:22:56 +0100

    remove main-method

--- MangaCtrl/src/main/java/de/herrlock/manga/html/ViewPageMain.java
+++ MangaCtrl/src/main/java/de/herrlock/manga/html/ViewPageMain.java
@@ -11,29 +11,22 @@ public final class ViewPageMain {
 
     public static void execute() {
         String input = JOptionPane.showInputDialog( "Input the folder where the chapters are." );
-        File folder = new File( input );
-        if ( folder.exists() ) {
-            ViewPage.execute( folder );
-        } else {
-            JOptionPane.showMessageDialog( null, "Folder does not exists." );
+        if ( input != null ) {
+            File folder = new File( input );
+            if ( folder.exists() ) {
+                ViewPage.execute( folder );
+            } else {
+                JOptionPane.showMessageDialog( null, "Folder does not exists." );
+            }
         }
     }
+
     /**
      * @param folder
      *            the mangafolder to create a viewer for (has the format {@code <manganame>_<timestamp>})
      */
     public static void execute( File folder ) {
         ViewPage.execute( folder );
-    }
-
-    /**
-     * only for testing
-     * 
-     * @param args
-     *            unused
-     */
-    public static void main( String[] args ) {
-        execute( new File( "./download/aoki_hagane_no_arpeggio_150306175215" ) );
     }
 
     private ViewPageMain() {
