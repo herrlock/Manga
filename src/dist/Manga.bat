d4065241f2d6a2890e9ad2cc300482605fdf8188
Jan Rau <jan.rau@itelligence.de>
2015-03-25 14:00:32 +0100

    update launch-files, add check on printed exception

--- src/dist/Manga.bat
+++ src/dist/Manga.bat
@@ -3,4 +3,14 @@ echo. > log/err.log
 echo ------------------------ >> log/err.log
 echo %date%, %time% >> log/err.log
 java -cp "lib/*" de.herrlock.manga.Ctrl 2>>log/err.log
-if errorlevel 1 notepad log/err.log
+if errorlevel 1 goto showerrlog
+
+for /f %%C in ('Find /V /C "" ^< log/err.log') do set Count=%%C
+if %Count% GTR 3 goto showerrlog
+
+goto end
+
+:showerrlog
+notepad log/err.log
+
+:end
