430e4c4d6581c08f9f5b4bd8cd792a132a0945b5
herrlock <herrlock@t-online.de>
2015-04-03 19:38:21 +0200

    add jdhome-textfield and add tooltips to labels

--- MangaLib/src/main/java/de/herrlock/manga/ui/main/Center.java
+++ MangaLib/src/main/java/de/herrlock/manga/ui/main/Center.java
@@ -4,18 +4,18 @@ import javafx.beans.value.ChangeListener;
 import javafx.scene.Node;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javafx.scene.control.Tooltip;
 import javafx.scene.layout.GridPane;
 import de.herrlock.javafx.scene.NodeContainer;
 
 class Center extends NodeContainer {
     @Override
     protected Node createNode() {
-        // TODO: build center
         int y = 0;
         final GridPane gridPane = new GridPane();
         {
             final Label lblTop = new Label();
-            lblTop.setPrefWidth( 150 );
+            lblTop.setPrefWidth( 200 );
             lblTop.setVisible( false );
             gridPane.add( lblTop, 0, y );
 
@@ -27,7 +27,9 @@ class Center extends NodeContainer {
 
         String lblPre = "center.label.";
         {
-            final Label lblUrl = new Label( MDGuiController.i18n.getString( lblPre + "url" ) );
+            String i18n_url = MDGuiController.i18n.getString( lblPre + "url" );
+            final Label lblUrl = new Label( i18n_url );
+            lblUrl.setTooltip( new Tooltip( i18n_url ) );
             gridPane.add( lblUrl, 0, y );
 
             final TextField tfUrl = new TextField();
@@ -39,7 +41,9 @@ class Center extends NodeContainer {
             y++ ;
         }
         {
-            final Label lblPattern = new Label( MDGuiController.i18n.getString( lblPre + "pattern" ) );
+            String i18n_pattern = MDGuiController.i18n.getString( lblPre + "pattern" );
+            final Label lblPattern = new Label( i18n_pattern );
+            lblPattern.setTooltip( new Tooltip( i18n_pattern ) );
             gridPane.add( lblPattern, 0, y );
 
             final TextField tfPattern = new TextField();
@@ -48,12 +52,25 @@ class Center extends NodeContainer {
             y++ ;
         }
         {
-            final Label lblProxyAddress = new Label( MDGuiController.i18n.getString( lblPre + "proxyaddress" ) );
+            String i18n_proxyaddress = MDGuiController.i18n.getString( lblPre + "proxyaddress" );
+            final Label lblProxyAddress = new Label( i18n_proxyaddress );
+            lblProxyAddress.setTooltip( new Tooltip( i18n_proxyaddress ) );
             gridPane.add( lblProxyAddress, 0, y );
 
             final TextField tfProxyAddress = new TextField();
             tfProxyAddress.setPromptText( "http://www.example.org:8080/proxy" );
             gridPane.add( tfProxyAddress, 1, y );
+            y++ ;
+        }
+        {
+            String i18n_jdhome = MDGuiController.i18n.getString( lblPre + "jdhome" );
+            final Label lblJDPath = new Label( i18n_jdhome );
+            lblJDPath.setTooltip( new Tooltip( i18n_jdhome ) );
+            gridPane.add( lblJDPath, 0, y );
+
+            final TextField tfJDPath = new TextField();
+            tfJDPath.setPromptText( "C:/Program Files/JDownloader/" );
+            gridPane.add( tfJDPath, 1, y );
             y++ ;
         }
         {
