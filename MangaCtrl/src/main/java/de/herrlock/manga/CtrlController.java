cb20507ed7d8f48ecfa23e92c5da5c9c5c8c6b9d
herrlock <herrlock@t-online.de>
2015-04-05 15:56:52 +0200

    remove the button's eventhandler when clicked on one of them

--- MangaCtrl/src/main/java/de/herrlock/manga/CtrlController.java
+++ MangaCtrl/src/main/java/de/herrlock/manga/CtrlController.java
@@ -5,6 +5,7 @@ import javafx.concurrent.Task;
 import javafx.concurrent.WorkerStateEvent;
 import javafx.event.EventHandler;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.text.Text;
 import de.herrlock.manga.downloader.DialogDownloader;
 import de.herrlock.manga.host.PrintAllHoster;
@@ -15,44 +16,46 @@ public class CtrlController {
 
     @FXML
     public Text runningText, bottomText;
+    @FXML
+    public Button btnStartDL, btnShowHosts, btnAddToJD, btnCreateHTML;
 
     public void clearText() {
         this.bottomText.setText( "" );
     }
 
     public void showTextStart() {
-        CtrlController.this.bottomText.setText( Ctrl.i18n.getString( "button.tooltip.startDL" ) );
+        setBottomText( "button.tooltip.startDL" );
     }
-
     public void showTextHosts() {
-        CtrlController.this.bottomText.setText( Ctrl.i18n.getString( "button.tooltip.showHosts" ) );
+        setBottomText( "button.tooltip.showHosts" );
     }
-
     public void showTextJD() {
-        CtrlController.this.bottomText.setText( Ctrl.i18n.getString( "button.tooltip.addToJD" ) );
+        setBottomText( "button.tooltip.addToJD" );
     }
-
     public void showTextHTML() {
-        CtrlController.this.bottomText.setText( Ctrl.i18n.getString( "button.tooltip.createHTML" ) );
+        setBottomText( "button.tooltip.createHTML" );
+    }
+    private void setBottomText( String i18nKey ) {
+        CtrlController.this.bottomText.setText( Ctrl.i18n.getString( i18nKey ) );
     }
 
     public void startDownload() {
         handleBtnClick( Exec.DIALOG_DOWNLOADER );
     }
-
     public void showHosts() {
         handleBtnClick( Exec.PRINT_ALL_HOSTER );
     }
-
     public void exportToJD() {
         handleBtnClick( Exec.ADD_TO_JD );
     }
-
     public void createHTML() {
         handleBtnClick( Exec.VIEW_PAGE_MAIN );
     }
-
     public void handleBtnClick( Exec exec ) {
+        this.btnStartDL.setOnAction( null );
+        this.btnShowHosts.setOnAction( null );
+        this.btnAddToJD.setOnAction( null );
+        this.btnCreateHTML.setOnAction( null );
         new Thread( new ExecHandlerTask( exec ) ).start();
     }
 
