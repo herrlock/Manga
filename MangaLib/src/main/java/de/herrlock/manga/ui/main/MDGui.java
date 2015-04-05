9cdeaa1eccb8145a070047bfb4a3c4427e50d82d
herrlock <herrlock@t-online.de>
2015-04-05 16:02:43 +0200

    move code to controller

--- MangaLib/src/main/java/de/herrlock/manga/ui/main/MDGui.java
+++ MangaLib/src/main/java/de/herrlock/manga/ui/main/MDGui.java
@@ -2,13 +2,9 @@ package de.herrlock.manga.ui.main;
 
 import java.util.Arrays;
 import java.util.Collection;
-import java.util.List;
 
 import javafx.application.Application;
-import javafx.beans.value.ChangeListener;
-import javafx.beans.value.ObservableValue;
 import javafx.scene.Scene;
-import javafx.scene.control.TextField;
 import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 import de.herrlock.javafx.AbstractApplication;
@@ -45,49 +41,5 @@ class MDGuiStage extends SceneContainer {
     @Override
     public String getTitle() {
         return "MangaDownloader";
-    }
-}
-
-final class EmptyListener implements ChangeListener<String> {
-    private final TextField textField;
-
-    public EmptyListener( TextField textField ) {
-        this.textField = textField;
-    }
-
-    @Override
-    public void changed( ObservableValue<? extends String> obsValue, String string1, String string2 ) {
-        List<String> classes = this.textField.getStyleClass();
-        if ( string2.trim().isEmpty() ) {
-            classes.add( CCN.ISEMPTY );
-        } else {
-            classes.remove( CCN.ISEMPTY );
-        }
-    }
-}
-
-/**
- * CSS-Classname constants
- */
-final class CCN {
-
-    public static final String MAGENTA = "magenta";
-    public static final String RED = "red";
-    public static final String YELLOW = "yellow";
-    public static final String GREEN = "green";
-    public static final String BLUE = "blue";
-    public static final String CYAN = "cyan";
-    public static final String GREY = "grey";
-
-    public static final String GRIDPANE = "gridpane";
-    public static final String TEXT = "text";
-
-    public static final String ISEMPTY = "isEmpty";
-
-    public static final String PADDING_8 = "padding8";
-    public static final String PADDING_16 = "padding16";
-
-    private CCN() {
-        // not used
     }
 }
--- MangaLib/src/main/java/de/herrlock/manga/ui/main/MDGuiController.java
+++ MangaLib/src/main/java/de/herrlock/manga/ui/main/MDGuiController.java
@@ -1,9 +1,13 @@
 package de.herrlock.manga.ui.main;
 
+import java.util.List;
 import java.util.ResourceBundle;
 
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
+import javafx.scene.control.TextField;
 
 /**
  * @author HerrLock
@@ -18,4 +22,48 @@ public class MDGuiController {
         }
     };
 
+}
+
+final class EmptyListener implements ChangeListener<String> {
+    private final TextField textField;
+
+    public EmptyListener( TextField textField ) {
+        this.textField = textField;
+    }
+
+    @Override
+    public void changed( ObservableValue<? extends String> obsValue, String string1, String string2 ) {
+        List<String> classes = this.textField.getStyleClass();
+        if ( string2.trim().isEmpty() ) {
+            classes.add( CCN.ISEMPTY );
+        } else {
+            classes.remove( CCN.ISEMPTY );
+        }
+    }
+}
+
+/**
+ * CSS-Classname constants
+ */
+final class CCN {
+
+    public static final String MAGENTA = "magenta";
+    public static final String RED = "red";
+    public static final String YELLOW = "yellow";
+    public static final String GREEN = "green";
+    public static final String BLUE = "blue";
+    public static final String CYAN = "cyan";
+    public static final String GREY = "grey";
+
+    public static final String GRIDPANE = "gridpane";
+    public static final String TEXT = "text";
+
+    public static final String ISEMPTY = "isEmpty";
+
+    public static final String PADDING_8 = "padding8";
+    public static final String PADDING_16 = "padding16";
+
+    private CCN() {
+        // not used
+    }
 }
