739baa998f5ded0d0f852854dc5613285c86447e
herrlock <herrlock@t-online.de>
2015-06-07 12:31:28 +0200

    allow more headers in Response

--- MangaLib/src/main/java/de/herrlock/manga/http/response/Response.java
+++ MangaLib/src/main/java/de/herrlock/manga/http/response/Response.java
@@ -1,12 +1,16 @@
 package de.herrlock.manga.http.response;
 
 import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Map.Entry;
 
 /**
  * @author HerrLock
  */
 public abstract class Response {
     private int code;
+    private Map<String, String> header = new HashMap<>();
 
     public Response() {
         this( -1 );
@@ -21,6 +25,11 @@ public abstract class Response {
         return this;
     }
 
+    public Response setHeader( String key, String value ) {
+        this.header.put( key, value );
+        return this;
+    }
+
     public abstract byte[] getData();
     protected abstract String getContentType();
 
@@ -29,9 +38,16 @@ public abstract class Response {
         sb.append( "HTTP/1.1 " );
         sb.append( this.code );
         sb.append( "\nContent-Type: " );
-        sb.append( getContentType() );
+        sb.append( this.header.containsKey( "Content-Type" ) ? this.header.get( "Content-Type" ) : getContentType() );
         sb.append( "\nDate: " );
-        sb.append( new Date().toString() );
+        sb.append( this.header.containsKey( "Date" ) ? this.header.get( "Date" ) : new Date().toString() );
+        for ( Entry<String, String> entry : this.header.entrySet() ) {
+            sb.append( "\n" );
+            sb.append( entry.getKey() );
+            sb.append( ": " );
+            sb.append( entry.getValue() );
+        }
+
         sb.append( "\n\n" );
         return sb.toString();
     }
