2907c5466b297782c64cd722fc1134c7bad598aa
Jan Rau <jan.rau@itelligence.de>
2015-06-09 17:52:27 +0200

    add tests for the crawljob-classes

---
+++ MangaLib/src/test/java/de/herrlock/manga/jd/TestCrawljob.java
@@ -0,0 +1,11 @@
+package de.herrlock.manga.jd;
+
+/**
+ * @author HerrLock
+ */
+public class TestCrawljob {
+    // @Test
+    // public void test1() {
+    // Crawljob c = new Crawljob( new File( "" ), "42" );
+    // }
+}
---
+++ MangaLib/src/test/java/de/herrlock/manga/jd/TestCrawljobEntry.java
@@ -0,0 +1,21 @@
+package de.herrlock.manga.jd;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * @author HerrLock
+ */
+public class TestCrawljobEntry {
+
+    @Test
+    public void testExport() {
+        CrawljobEntry crawljobEntry = new CrawljobEntry( "filename", "url" );
+        String export = crawljobEntry.export();
+        String[] split = export.split( System.lineSeparator() );
+        String[] expected = {
+            "text=url", "filename=filename.jpg"
+        };
+        Assert.assertArrayEquals( expected, split );
+    }
+}
