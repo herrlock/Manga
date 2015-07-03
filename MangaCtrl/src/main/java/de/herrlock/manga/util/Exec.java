d44702e7da7a3b6ca230b462e53e9d8cab323967
herrlock <herrlock@t-online.de>
2015-07-03 15:26:29 +0200

    change Exec from enum to abstract class to allow extending

--- MangaCtrl/src/main/java/de/herrlock/manga/util/Exec.java
+++ MangaCtrl/src/main/java/de/herrlock/manga/util/Exec.java
@@ -2,48 +2,48 @@ package de.herrlock.manga.util;
 
 import de.herrlock.manga.downloader.DialogDownloader;
 import de.herrlock.manga.downloader.GUIDownloader;
+import de.herrlock.manga.downloader.PlainDownloader;
 import de.herrlock.manga.host.PrintAllHoster;
 import de.herrlock.manga.html.ViewPageMain;
 import de.herrlock.manga.jd.JDExport;
 
-public enum Exec {
-    DIALOG_DOWNLOADER {
+public abstract class Exec {
+    public static final Exec DIALOG_DOWNLOADER = new Exec() {
         @Override
         public void execute() {
             DialogDownloader.execute();
         }
-    },
-    GUI_DOWNLOADER {
+    }, PLAIN_DOWNLOADER = new Exec() {
+        @Override
+        public void execute() {
+            PlainDownloader.execute();
+        }
+    }, GUI_DOWNLOADER = new Exec() {
         @Override
         public void execute() {
             GUIDownloader.execute();
         }
-    },
-    ADD_TO_JD_W_FILE {
+    }, ADD_TO_JD_W_FILE = new Exec() {
         @Override
         public void execute() {
             JDExport.executeGetFileProperties();
         }
-    },
-    ADD_TO_JD_W_GUI {
+    }, ADD_TO_JD_W_GUI = new Exec() {
         @Override
         public void execute() {
             JDExport.executeGetGuiProperties();
         }
-    },
-    VIEW_PAGE_MAIN {
+    }, VIEW_PAGE_MAIN = new Exec() {
         @Override
         public void execute() {
             ViewPageMain.execute();
         }
-    },
-    PRINT_ALL_HOSTER {
+    }, PRINT_ALL_HOSTER = new Exec() {
         @Override
         public void execute() {
             PrintAllHoster.execute();
         }
-    },
-    DO_NOTHING {
+    }, DO_NOTHING = new Exec() {
         @Override
         public void execute() {
             // do nothing :)
