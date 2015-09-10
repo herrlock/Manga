8c6d632111fcb2de2f7bdd7188c0ad680d3e128e
herrlock <herrlock@t-online.de>
2015-09-10 22:02:49 +0200

    add javadoc

--- MangaLib/src/main/java/de/herrlock/manga/exceptions/InitializeException.java
+++ MangaLib/src/main/java/de/herrlock/manga/exceptions/InitializeException.java
@@ -7,15 +7,41 @@ package de.herrlock.manga.exceptions;
  */
 public class InitializeException extends RuntimeException {
 
+    /**
+     * A new InitializeException without detailed message or cause
+     */
     public InitializeException() {
         super();
     }
+
+    /**
+     * A new InitializeException with a detailed message
+     * 
+     * @param message
+     *            a detailmessage why this exception occurred
+     */
     public InitializeException( String message ) {
         super( message );
     }
+
+    /**
+     * A new InitializeException with a cause
+     * 
+     * @param cause
+     *            the cause of this exception
+     */
     public InitializeException( Exception cause ) {
         super( cause );
     }
+
+    /**
+     * A new InitializeException with a detailed message and cause
+     * 
+     * @param message
+     *            a detailmessage why this exception occurred
+     * @param cause
+     *            the cause of this exception
+     */
     public InitializeException( String message, Exception cause ) {
         super( message, cause );
     }
