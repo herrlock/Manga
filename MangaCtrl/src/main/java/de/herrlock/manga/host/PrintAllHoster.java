01ac390498a9000114326877857a9d2bfc18475b
herrlock <herrlock@t-online.de>
2015-10-03 17:30:51 +0200

    log the Hoster to the Logger by default

--- MangaCtrl/src/main/java/de/herrlock/manga/host/PrintAllHoster.java
+++ MangaCtrl/src/main/java/de/herrlock/manga/host/PrintAllHoster.java
@@ -3,12 +3,18 @@ package de.herrlock.manga.host;
 import java.io.PrintStream;
 import java.util.Collection;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 /**
  * @author HerrLock
  */
 public final class PrintAllHoster {
+
+    private static final Logger logger = LogManager.getLogger();
+
     public static void execute() {
-        printHoster( System.out );
+        printHoster();
     }
 
     public static void printHoster( PrintStream out ) {
@@ -18,6 +24,15 @@ public final class PrintAllHoster {
             out.println( h );
         }
     }
+
+    public static void printHoster() {
+        logger.info( "availabile hoster" );
+        Collection<Hoster> values = Hosters.sortedValues();
+        for ( Hoster h : values ) {
+            logger.info( h );
+        }
+    }
+
     private PrintAllHoster() {
         // nothing to do
     }
