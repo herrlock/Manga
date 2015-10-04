99012cf637bdeb4409010f5fa1304827ed8c01b2
herrlock <herrlock@t-online.de>
2015-10-04 11:55:28 +0200

    update javadoc

--- MangaLib/src/main/java/de/herrlock/javafx/scene/SceneContainer.java
+++ MangaLib/src/main/java/de/herrlock/javafx/scene/SceneContainer.java
@@ -4,8 +4,6 @@ import java.util.Arrays;
 import java.util.Collection;
 import java.util.List;
 
-import javax.swing.Icon;
-
 import javafx.scene.Scene;
 import javafx.scene.image.Image;
 
@@ -35,7 +33,7 @@ public abstract class SceneContainer {
     }
 
     /**
-     * A {@link List} of {@link Icon}s to show. Can be Overridden to define own Icons; defaults to an empty List
+     * A {@link List} of {@link Image}s to show. Can be Overridden to define own Icons; defaults to an empty List
      * 
      * @return the icons to use
      */
