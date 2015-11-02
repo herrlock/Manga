9d56f9ae75961e3c253c5e67b675eb228e334b06
herrlock <herrlock@t-online.de>
2015-11-02 22:57:14 +0100

    add default-method

--- MangaLib/src/main/java/de/herrlock/javafx/NoWindowApplication.java
+++ MangaLib/src/main/java/de/herrlock/javafx/NoWindowApplication.java
@@ -4,6 +4,10 @@ import javafx.stage.Stage;
 
 public abstract class NoWindowApplication extends AbstractApplication {
 
+    public void start() {
+        this.start( null );
+    }
+
     @Override
     public abstract void start( final Stage stage );
 }
