c6303e765be0fc1150dbe0550aead82ca5911d37
herrlock <herrlock@t-online.de>
2016-01-03 21:20:09 +0100

    add test for MyOptions

---
+++ src/test/java/de/herrlock/manga/TestMyOptions.java
@@ -0,0 +1,23 @@
+package de.herrlock.manga;
+
+import org.apache.commons.cli.Options;
+import org.junit.Assert;
+import org.junit.Test;
+
+public final class TestMyOptions {
+
+    @Test
+    public void testOptionCount() {
+        MyOptions myOptions = new MyOptions();
+        Options options = myOptions.getOptions();
+        Assert.assertEquals( 11, options.getOptions().size() );
+    }
+
+    @Test
+    public void testRequiredOptionCount() {
+        MyOptions myOptions = new MyOptions();
+        Options options = myOptions.getOptions();
+        Assert.assertEquals( 1, options.getRequiredOptions().size() );
+    }
+
+}
