115e8ec702e4772a4f05e75905d522b11a0258cd
herrlock <herrlock@t-online.de>
2016-02-21 17:13:07 +0100

    check for arguments in a simpler way

--- src/dist/MangaLauncher.sh
+++ src/dist/MangaLauncher.sh
@@ -1,10 +1,11 @@
 #!/bin/sh
 
-if [[ $* -eq 0 ]] 
+# check for existence of at least one argument
+if [ "$1" ]
 then
-    # show help since no arguments are given
-    java -jar MangaLauncher.jar --help
-else
     # start MangaLauncher with the given arguments
     java -jar MangaLauncher.jar $*
+else
+    # show help since no arguments are given
+    java -jar MangaLauncher.jar --help
 fi
