d747de333b98583929b257ef68f7662303b19e54
herrlock <herrlock@t-online.de>
2016-07-05 18:14:57 +0200

    pack all jars with Pack200 and gzip, unpack the jars before running

---
+++ src/main/java/de/herrlock/manga/UnpackerMain.java
@@ -0,0 +1,56 @@
+package de.herrlock.manga;
+
+import java.io.IOException;
+import java.nio.file.DirectoryStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.jar.JarOutputStream;
+import java.util.jar.Pack200;
+import java.util.jar.Pack200.Unpacker;
+import java.util.zip.GZIPInputStream;
+
+/**
+ * @author HerrLock
+ */
+public class UnpackerMain {
+    public static void main( final String... args ) {
+        // check if packed archives exist
+        unpackArchives();
+        // call "real" main-class after the unpacking was successful
+        Main.main( args );
+    }
+
+    /**
+     * Unpacks all files ending with .jar.pack.gz with first gzip and second unpack200
+     * 
+     * @throws IOException
+     */
+    private static void unpackArchives() {
+        // check the lib-folder
+        Path lib = Paths.get( ".", "lib" );
+        // create an unpacker
+        Unpacker unpacker = Pack200.newUnpacker();
+        // find all ".jar.pack.gz"-files
+        try ( DirectoryStream<Path> directoryStream = Files.newDirectoryStream( lib, "*.jar.pack.gz" ) ) {
+            // iterate over the packed files
+            for ( Path packedFile : directoryStream ) {
+                String oldFileName = packedFile.toFile().getName();
+                // get the new filename
+                String newFileName = oldFileName.replace( ".pack.gz", "" );
+                Path newFile = lib.resolve( newFileName );
+                try ( GZIPInputStream in = new GZIPInputStream( Files.newInputStream( packedFile ) ) ) {
+                    try ( JarOutputStream out = new JarOutputStream( Files.newOutputStream( newFile ) ) ) {
+                        System.out.println( "Unpacking " + oldFileName + " to " + newFileName );
+                        // unpack the packed and gzipped file
+                        unpacker.unpack( in, out );
+                    }
+                }
+                // delete the old file if unpacking was successful
+                Files.delete( packedFile );
+            }
+        } catch ( IOException ex ) {
+            throw new IllegalStateException( "Cannot unpack archives", ex );
+        }
+    }
+}
