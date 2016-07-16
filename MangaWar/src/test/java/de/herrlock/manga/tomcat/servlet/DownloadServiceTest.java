9200a776eacaa13614ee1e3fd305662662141722
herrlock <herrlock@t-online.de>
2016-07-16 17:45:11 +0200

    add test for classes inside the DownloadService

---
+++ MangaWar/src/test/java/de/herrlock/manga/tomcat/servlet/DownloadServiceTest.java
@@ -0,0 +1,38 @@
+package de.herrlock.manga.tomcat.servlet;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertTrue;
+
+import java.util.UUID;
+
+import org.junit.Test;
+
+import de.herrlock.manga.tomcat.servlet.DownloadService.MDObject;
+import de.herrlock.manga.tomcat.servlet.DownloadService.ProgressObject;
+
+/**
+ * @author HerrLock
+ */
+public class DownloadServiceTest {
+    // private final DownloadService downloadServlet = new DownloadService();
+
+    @Test
+    public void testProgressObject() {
+        UUID randomUUID = UUID.randomUUID();
+        ProgressObject progressObject = new ProgressObject( randomUUID, "url", true, 42, 1337 );
+        assertEquals( randomUUID, progressObject.getUuid() );
+        assertEquals( "url", progressObject.getUrl() );
+        assertTrue( progressObject.getStarted() );
+        assertEquals( 42, progressObject.getProgress() );
+        assertEquals( 1337, progressObject.getMaxProgress() );
+    }
+
+    @Test
+    public void testMDObject() {
+        MDObject mdObject = new MDObject( "", null );
+        assertEquals( "", mdObject.getUrl() );
+        assertNull( mdObject.getMdownloader() );
+    }
+
+}
