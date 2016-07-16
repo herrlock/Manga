9f70bba9d64ad99c45f74a1881c6df96bb5a8b79
herrlock <herrlock@t-online.de>
2016-07-16 17:43:29 +0200

    update test

--- MangaWar/src/test/java/de/herrlock/manga/tomcat/servlet/ImageServletTest.java
+++ MangaWar/src/test/java/de/herrlock/manga/tomcat/servlet/ImageServletTest.java
@@ -1,19 +1,38 @@
 package de.herrlock.manga.tomcat.servlet;
 
+import static org.junit.Assert.assertEquals;
+
 import javax.ws.rs.core.Response;
 
-import org.junit.Assert;
 import org.junit.Test;
+
+import de.herrlock.manga.tomcat.servlet.ImageService.BGObject;
+import de.herrlock.manga.tomcat.servlet.ImageService.Image;
 
 /**
  * @author HerrLock
  */
 public class ImageServletTest {
-    private final ImageService imageServlet = new ImageService();
+    private final ImageService imageService = new ImageService();
 
     @Test
     public void getImage() {
-        Response response = this.imageServlet.getImage();
-        Assert.assertEquals( 200, response.getStatus() );
+        Response response = this.imageService.getImage();
+        assertEquals( 200, response.getStatus() );
     }
+
+    @Test
+    public void getBGObjectWhite() {
+        BGObject bgObject = new BGObject( Image.SAKURASOU_SHIINA );
+        assertEquals( "#ffffff", bgObject.getColor() );
+        assertEquals( "/res/bg/sakurasou_shiina.jpg", bgObject.getPath() );
+    }
+
+    @Test
+    public void getBGObjectBlack() {
+        BGObject bgObject = new BGObject( Image.ONEPIECE_JOLLYROGER );
+        assertEquals( "#000000", bgObject.getColor() );
+        assertEquals( "/res/bg/onepiece_jollyroger.jpg", bgObject.getPath() );
+    }
+
 }
