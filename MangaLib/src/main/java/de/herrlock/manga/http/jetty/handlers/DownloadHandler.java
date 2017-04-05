92a21ae40097484c84bfe8aef2bfecdaf285af70
herrlock <herrlock@t-online.de>
2017-04-05 22:16:46 +0200

    create UUID after the MDownloader is instantiated and added to the queue

--- MangaLib/src/main/java/de/herrlock/manga/http/jetty/handlers/DownloadHandler.java
+++ MangaLib/src/main/java/de/herrlock/manga/http/jetty/handlers/DownloadHandler.java
@@ -100,10 +100,9 @@ final class StartDownloadHandler extends AbstractHandler {
         final DownloadConfiguration conf = DownloadConfiguration.create( p );
 
         final MDownloader newDownloader = new PlainDownloader( conf );
-        UUID randomUUID = this.dlContext.put( new MDObject( url, newDownloader ) );
-
         DownloadProcessor.getInstance().addDownload( newDownloader );
 
+        UUID randomUUID = this.dlContext.put( new MDObject( url, newDownloader ) );
         response.getWriter().write( randomUUID.toString() );
         response.setContentType( MediaType.PLAIN_TEXT_UTF_8.toString() );
         response.setStatus( HttpServletResponse.SC_OK );
