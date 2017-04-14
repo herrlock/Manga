7062896b34343610d1cafa06e30c0b73779e0383
herrlock <herrlock@t-online.de>
2017-04-14 23:46:14 +0200

    make sure the arguments are passed as intended

--- src/main/dist/MangaLauncher.sh
+++ src/main/dist/MangaLauncher.sh
@@ -1,10 +1,11 @@
 #!/bin/sh
+set -x
 
 # check for existence of at least one argument
 if [ "$1" ]
 then
     # start MangaLauncher with the given arguments
-    java -jar MangaLauncher.jar $*
+    java -jar MangaLauncher.jar "$@"
 else
     # show help since no arguments are given
     java -jar MangaLauncher.jar --help
