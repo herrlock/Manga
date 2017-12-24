4d26d023c9315f31c21176362c74ed7d3d79eb01
herrlock <herrlock@t-online.de>
2017-12-24 18:35:34 +0100

    add viewpage-option 'clear'

--- src/main/java/de/herrlock/manga/cli/options/ViewpageOptions.java
+++ src/main/java/de/herrlock/manga/cli/options/ViewpageOptions.java
@@ -29,9 +29,13 @@ public final class ViewpageOptions extends SubOptions {
         .argName( "format" ) //
         .desc( "Create a comicbook-archive in the given format [CBZ, CBT]" ) //
         .build();
-    private final Option cleanOption = Option.builder() //
+    private final Option cleanOption = Option.builder( "c" ) //
         .longOpt( "clean" ) //
         .desc( "Whether to remove the downloaded chapters after archiving them" ) //
+        .build();
+    private final Option clearOption = Option.builder() //
+        .longOpt( "clear" ) //
+        .desc( "Same as \"clean\"" ) //
         .build();
 
     private final OptionGroup typeGroup = new OptionGroup() //
@@ -50,6 +54,7 @@ public final class ViewpageOptions extends SubOptions {
             .addOptionGroup( this.typeGroup ) //
             .addOption( this.folderOption ) //
             .addOption( this.cleanOption ) //
+            .addOption( this.clearOption ) //
         ;
     }
 
